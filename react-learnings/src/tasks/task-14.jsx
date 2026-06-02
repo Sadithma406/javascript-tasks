7fb254eee264f29857fb23a9f09e528aef205bf7
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-06-02 15:07:48 +0530

    task 14: alert functionality

---
+++ react-learnings/src/tasks/task-14.jsx
@@ -0,0 +1,10 @@
+function Task14() {
+    function myFunction() {
+        alert("You clicked me!");
+    }
+    return (
+        <button onClick={myFunction}>Click me</button>
+    );
+}
+
+export default Task14;
